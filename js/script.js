120c011f263f8228080d7f5db898c25fde62e022
stodipo <odipo87@gmail.com>
2018-10-27 06:50:28 +0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var name="Stephen";
+var age=21;
+console.log(name);
+//alert(name);
+//prompt("What is your name?");
+
+function firstFunction(){
+    console.log(name+" "+age);
+};
+//firstFunction();
+function displayText(){
+    document.getElementById("quote").innerHTML="Only the Paranoid Survive.";
+}
